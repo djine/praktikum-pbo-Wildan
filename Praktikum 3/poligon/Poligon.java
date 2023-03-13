b3db68878ff6d8dcb7ba2e5ddacf4144819910c0
Wildan Adji <91516723+djine@users.noreply.github.com>
2023-03-13 10:33:25 +0700

    Add files via upload

---
+++ Praktikum 3/poligon/Poligon.java
@@ -0,0 +1,14 @@
+// File     : Poligon.java 02/04/2023
+// Penulis  : Wildan Adji Prabowo
+// Deskripsi: Representasi dasar dari objek poligon (segi banyak)
+
+package org.poligon;
+
+public class Poligon {
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+	
+}
